1635d01b798b4eaccd4a33ae54416a0a60c9031a
Diogo Andrade <diogo.g.andrade@gmail.com>
2019-10-05 10:56:19 +0100

    Updated examples

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 def draw_graph_axis_x(screen, x, y, x_inc, x_count, div_size, color, font = None, values = None):
     current_x = x
@@ -57,3 +58,49 @@ def draw_triangle(screen, p1, p2, p3, color, width):
     pygame.draw.line(screen, color, p1, p2, width)
     pygame.draw.line(screen, color, p2, p3, width)
     pygame.draw.line(screen, color, p3, p1, width)
+
+def get_dir(p1, p2):
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+    n = math.sqrt(dx*dx + dy*dy)
+
+    return (dx / n, dy / n), n
+
+def draw_vector(screen, color, p1, p2, width, font = None, name = "", offset = 10):
+    pygame.draw.line(screen, color, p1, p2, width)
+
+    dir, length = get_dir(p1, p2)
+    perp = (-dir[1], dir[0])
+
+    p3 = (p2[0] - dir[0] * 10 + perp[0] * 10, p2[1] - dir[1] * 10 + perp[1] * 10)
+    p4 = (p2[0] - dir[0] * 10 - perp[0] * 10, p2[1] - dir[1] * 10 - perp[1] * 10)
+
+    pygame.draw.line(screen, color, p2, p3, width)
+    pygame.draw.line(screen, color, p2, p4, width)
+
+    if (font != None):
+        c = (p1[0] + dir[0] * length * 0.5 - perp[0] * offset, p1[1] + dir[1] * length * 0.5 - perp[1] * offset)
+        font.render_to(screen, c, name, color)
+
+def draw_dashed_line(screen, color, p1, p2, width, n_dash, font = None, name ="", offset = 10):
+    dir, length = get_dir(p1, p2)
+    p = p1
+
+    l = 0.5 * length / n_dash
+
+    for i in range(0, n_dash):
+        pygame.draw.line(screen, color, p, (p[0] + dir[0] * l, p[1] + dir[1] * l), width)
+        p = (p[0] + dir[0] * l * 2, p[1] + dir[1] * l * 2)
+
+    if (font != None):
+        c = (p1[0] + dir[0] * length * 0.5, p1[1] + dir[1] * length * 0.5 - offset)
+        font.render_to(screen, c, name, color)
+
+def draw_label(screen, color, p, font, name):
+    if (font != None):
+        font.render_to(screen, p, name, color)
+
+def draw_point(screen, color, p, size, font = None, name = "", offset = 10):
+    pygame.draw.circle(screen, color, p, size)
+    if (font != None):
+        draw_label(screen, color, (p[0] + offset, p[1]), font, name)
